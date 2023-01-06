27d5426cd8ea6964f7f11c2d54bc164154fa8b4f
vinayakgarole <vinayakgarole10@gamil.com>
2023-01-06 10:23:02 +0530

    Added: Set event listeners when document is loaded

--- JS/EmployeePayrollForm.js
+++ JS/EmployeePayrollForm.js
@@ -1,6 +1,25 @@
-const salary = document.querySelector("#salary");
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('inpur', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+
+
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
 })
